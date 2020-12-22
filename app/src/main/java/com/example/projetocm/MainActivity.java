9310ebf60cc050bc5232d4940a5fe4618100e225
gon-ff <goncalofrancisco.ferreira@gmail.com>
2020-12-22 17:41:03 +0000

    main screen update

--- app/src/main/java/com/example/projetocm/MainActivity.java
+++ app/src/main/java/com/example/projetocm/MainActivity.java
@@ -25,7 +25,7 @@ import java.net.URLConnection;
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements FirstFragment.FirstFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity implements FirstFragment.FirstFragmentInteractionListener {
 
 
     @Override
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.Fir
     }
 
 
-
     @Override
     public void FirstFragmentInteraction() {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
@@ -49,77 +48,4 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.Fir
         fragmentTransaction.commit();
     }
 
-    /*
-    public void randomMeal(View view2) {
-        View view = findViewById(android.R.id.content).getRootView();
-        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
-        String cmd = spinner.getSelectedItem().toString();
-        String text = ((EditText)findViewById(R.id.editText)).getText().toString();
-
-        new Task().execute(cmd, text);
-    }
-    */
 }
-/*
-class Task extends AsyncTask<String, Void, Void> {
-
-    @Override
-    protected Void doInBackground(String... args) {
-        try {
-            switch (args[0]) {
-                case "meal name":
-                    Meal[] meals = API.searchMealByName(args[1]);
-                    for (Meal m : meals) {
-                        m.print();
-                    }
-                    break;
-                case "meal id":
-                    Meal m = API.searchMealById(args[1]);
-                    m.print();
-                    break;
-                case "list categories":
-                    Category[] categories = API.listCategories();
-                    for (Category c : categories) {
-                        c.print();
-                    }
-                    break;
-                case "list area":
-                    Area[] areas = API.listAreas();
-                    for (Area a : areas) {
-                        a.print();
-                    }
-                    break;
-                case "list ingredients":
-                    Ingredient[] ingredients = API.listIngredients();
-                    for (Ingredient i : ingredients) {
-                        i.print();
-                    }
-                    break;
-                case "filter ingredient":
-                    Meal[] meals2 = API.filterByIngredient(args[1]);
-                    for (Meal m2 : meals2) {
-                        m2.print();
-                    }
-                    break;
-                case "filter category":
-                    Meal[] meals3 = API.filterByCategory(args[1]);
-                    for (Meal m3 : meals3) {
-                        m3.print();
-                    }
-                    break;
-                case "filter area":
-                    Meal[] meals4 = API.filterByArea(args[1]);
-                    for (Meal m4 : meals4) {
-                        m4.print();
-                    }
-                    break;
-                default:
-                    System.out.println("no match");
-            }
-        } catch (NullPointerException e){
-            System.out.println("Cant find in API");
-        }
-        return null;
-    }
-}
-*/
